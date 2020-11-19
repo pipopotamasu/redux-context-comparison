3bb6c8c3bd41016e98352b2b1db90eeea17c46d6
pipopotamasu <murakami@vis-its.com>
2020-11-19 11:02:56 +0900

    wip

--- src/pages/ReduxExample.js
+++ src/pages/ReduxExample.js
@@ -1,3 +1,35 @@
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+const initialUserState = {
+  id: 1,
+  name: 'test name',
+  role: 'admin'
+};
+
+const userReducer = (state = initialUserState, action) => {
+  switch (action.type) {
+    case 'UPDATE_NAME':
+      return {
+        ...state,
+        name: action.payload
+      }
+    default:
+      return state;
+  }
+}
+
+const reducers = combineReducers({
+  user: userReducer,
+  // posts: posts,
+});
+
+const store = createStore(reducers);
+
 export default function ReduxExample () {
-  return <>Redux</>
+  return (
+    <Provider store={store}>
+      Redux
+    </Provider>
+  )
 }
