b5d46ed1ab851e079d03e0933804b5033941e7ae
pipopotamasu <ec0156hx@gmail.com>
2020-11-29 17:23:33 +0900

    add context example

--- src/pages/ContextExample.js
+++ src/pages/ContextExample.js
@@ -1,33 +1,55 @@
-import { createContext, useContext, useReducer } from 'react';
+import { useContext, useReducer } from 'react';
+import { initialState, reducer, Context } from '../context';
 import classes from './page.module.css';
-
-const state = {
-  user: { id: 1, name: 'test name' },
-  posts: [{ id: 1, title: 'test post' }]
-}
-
-const Context = createContext(state);
 
 function UserInfo () {
   console.log('UserInfo is updated!');
-  const { user } = useContext(Context);
+  const { user, dispatch } = useContext(Context);
 
   return (
     <div className={classes.userContainer}>
       <p>{user.name}</p>
-      <button>
+      <button
+        onClick={() => dispatch({ type: 'UPDATE_NAME', payload: 'updated name' })}
+      >
         update user
       </button>
     </div>
   )
 }
 
-export default function ContextExample () {
+function PostList () {
+  console.log('PostList is updated!');
+  const { posts, dispatch } = useContext(Context);
+
   return (
-    <Context.Provider value={state}>
+    <div className={classes.postsContainer}>
+      <ul>
+        {
+          posts.map(post => <li key={post.id}>{post.title}</li>)
+        }
+      </ul>
+      <button
+        onClick={() =>  {
+          dispatch(
+            { type: 'ADD_POST', payload: { id: posts.length + 1, title: 'added post' }}
+          )
+        }}
+      >
+        add post
+      </button>
+    </div>
+  )
+}
+
+export default function ContextExample () {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Context.Provider value={{ user: state.user, posts: state.posts, dispatch }}>
       <h1>Context</h1>
       <UserInfo />
-      {/* <PostList /> */}
+      <PostList />
     </Context.Provider>
   )
 }
