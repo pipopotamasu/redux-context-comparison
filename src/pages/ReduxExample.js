daa024773572b9ad125cebd2ebbfb59a1a6bf571
pipopotamasu <ec0156hx@gmail.com>
2020-11-29 11:10:46 +0900

    wip

--- src/pages/ReduxExample.js
+++ src/pages/ReduxExample.js
@@ -1,11 +1,14 @@
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
 const initialUserState = {
   id: 1,
   name: 'test name',
-  role: 'admin'
 };
+
+const initialPostsState = [
+  { id: 1, title: 'test post' },
+]
 
 const userReducer = (state = initialUserState, action) => {
   switch (action.type) {
@@ -19,17 +22,52 @@ const userReducer = (state = initialUserState, action) => {
   }
 }
 
+const postsReducer = (state = initialPostsState, action) => {
+  switch (action.type) {
+    case 'ADD_POST':
+      return [...state, action.payload]
+    default:
+      return state;
+  }
+}
+
 const reducers = combineReducers({
   user: userReducer,
-  // posts: posts,
+  posts: postsReducer,
 });
 
 const store = createStore(reducers);
 
+function UserInfo () {
+  console.log('UserInfo is updated!');
+  const user = useSelector(state => state.user);
+
+  return (
+    <>
+      <p>{user.name}</p>
+    </>
+  )
+}
+
+function PostList () {
+  console.log('PostList is updated!');
+  const posts = useSelector(state => state.posts);
+
+  return (
+    <ul>
+      {
+        posts.map(post => <li key={post.id}>{post.title}</li>)
+      }
+    </ul>
+  )
+}
+
 export default function ReduxExample () {
   return (
     <Provider store={store}>
-      Redux
+      <h1>Redux</h1>
+      <UserInfo />
+      <PostList />
     </Provider>
   )
 }
