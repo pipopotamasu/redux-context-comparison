ee9368041e79dc06bf374b5ede0e3514e054424d
pipopotamasu <ec0156hx@gmail.com>
2020-11-29 17:06:39 +0900

    wip

--- src/pages/ContextExample.js
+++ src/pages/ContextExample.js
@@ -1,3 +1,33 @@
+import { createContext, useContext, useReducer } from 'react';
+import classes from './page.module.css';
+
+const state = {
+  user: { id: 1, name: 'test name' },
+  posts: [{ id: 1, title: 'test post' }]
+}
+
+const Context = createContext(state);
+
+function UserInfo () {
+  console.log('UserInfo is updated!');
+  const { user } = useContext(Context);
+
+  return (
+    <div className={classes.userContainer}>
+      <p>{user.name}</p>
+      <button>
+        update user
+      </button>
+    </div>
+  )
+}
+
 export default function ContextExample () {
-  return <>Context</>
+  return (
+    <Context.Provider value={state}>
+      <h1>Context</h1>
+      <UserInfo />
+      {/* <PostList /> */}
+    </Context.Provider>
+  )
 }
